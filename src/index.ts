a27af4fc05e2eac4fbff18efaad9aa5e1b0d9293
Niall Moore <niall@watr.org>
2022-11-07 10:19:27 +0000

    feat(evm): add support for eth addresses

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda'
 import { Update } from "node-telegram-bot-api"
 import WatrBot from "./telegram/WatrBot"
 import { MessageType } from "./telegram/MessageType"
+import { evmToAddress, isEthereumAddress } from "@polkadot/util-crypto"
 
 export const lambdaHandler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     const liveMode = process.env.LIVE_MODE?.toUpperCase() == "TRUE"
     const bot = new WatrBot(process.env.TELEGRAM_TOKEN ?? "", liveMode)
     const body = event.body
     const update: Update = JSON.parse(body ?? "")
-    const [valid, address] = bot.parse(update)
+    let [valid, address] = bot.parse(update)
     if (valid && address != null) {
         bot.send(update, MessageType.WORKING)
         const faucet = new Faucet()
+        if (isEthereumAddress(address)) {
+            address = evmToAddress(address)
+        } 
         const result = await faucet.send(address)
         if (result) {
             bot.send(update, MessageType.COMPLETE)
